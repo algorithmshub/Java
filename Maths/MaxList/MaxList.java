cbdf021653159aed1104eca22c5283e9c13592ee
Bunlong <bunlong.van@gmail.com>
2020-11-09 22:37:04 +0700

    :rocket: MaxList

---
+++ Maths/MaxList/MaxList.java
@@ -0,0 +1,31 @@
+/**
+  * Author
+  *
+  * Bunlong @AlgorithmsHub 
+  */
+public class MaxList {
+
+  /** Driver Code */
+  public static void main(String[] args) {
+    int result = max(new int[]{ 12, 35, 7, 10, 34, 2 });
+    System.out.println(result);
+  }
+
+  /**
+    * Returns the bigger of a {@code int} value in a list.
+    *
+    * @param a an argument.
+    * @return the bigger of {@code a}.
+    */
+  public static int max(int[] a) {
+    int maxValue = a[0];
+
+    for (int i = 1; i < a.length; i++) {
+      if (a[i] > maxValue) {
+        maxValue = a[i];
+      }
+    }
+
+    return maxValue;
+  }
+}
--- Maths/MinList/README.md
+++ Maths/MinList/README.md
@@ -40,5 +40,4 @@ public class MinList {
     return minValue;
   }
 }
-
 ```

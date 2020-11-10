fa2562dc7f8c397c55494c514c97b3383cac9918
Bunlong <bunlong.van@gmail.com>
2020-11-10 21:34:22 +0700

    :recycle: Refactor

--- Maths/MinList/MinList.java
+++ Maths/MinList/MinList.java
@@ -7,17 +7,17 @@ public class MinList {
 
   /** Driver Code */
   public static void main(String[] args) {
-    int result = min(new int[]{ 12, 35, 7, 10, 34, 2 });
+    int result = findMin(new int[]{ 12, 35, 7, 10, 34, 2 });
     System.out.println(result);
   }
 
   /**
-    * Returns the smaller of a {@code int} value in a list.
+    * find the minimum number of an array of numbers
     *
-    * @param a an argument.
-    * @return the smaller of {@code a}.
+    * @param a the array contains element
+    * @return min value
     */
-  public static int min(int[] a) {
+  public static int findMin(int[] a) {
     int minValue = a[0];
 
     for (int i = 1; i < a.length; i++) {

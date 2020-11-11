d5af45b451149bcfd043aa49fa44d916b37d0abe
Bunlong <bunlong.van@gmail.com>
2020-11-11 23:10:13 +0700

    :heavy_plus_sign: MinAbsoluteList

---
+++ Maths/MinAbsoluteList/MinAbsoluteList.java
@@ -0,0 +1,48 @@
+/**
+ * Author
+ *
+ * Bunlong @AlgorithmsHub 
+ */
+
+/**
+ * Expected
+ *
+ * findMinAbsolute([0, -5, -1, 11]) === 0
+ * findMinAbsolute([7 , -17, -9]) === 7
+ */
+public class MinAbsoluteList {
+
+  /** Driver Code */
+  public static void main(String[] args) {
+    System.out.println(findMinAbsolute(new int[]{ 0, -5, -1, 11 }) == 0);
+    System.out.println(findMinAbsolute(new int[]{ 7 , -17, -9 }) == 7);
+  }
+
+  /**
+   * get the value, return the absolute min value
+   *
+   * @param a contains elements
+   * @return the absolute min value
+   */
+  public static int findMinAbsolute(int[] a) {
+    int minAbsValue = a[0];
+
+    for (int i = 1, length = a.length; i < length; ++i) {
+      if (absVal(a[i]) < absVal(minAbsValue)) {
+        minAbsValue = a[i];
+      }
+    }
+
+    return minAbsValue;
+  }
+
+  /**
+   * if value is less than zero, make value positive
+   *
+   * @param value a number
+   * @return the absolute value of a number
+   */
+  public static int absVal(int value) {
+    return value < 0 ? -value : value;
+  }
+}

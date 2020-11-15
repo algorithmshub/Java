35f0c6812e30f6b243bf92c1a2179ec1c9047e19
Bunlong <bunlong.van@gmail.com>
2020-11-15 22:45:50 +0700

    :heavy_plus_sign: AllCharactersSame

---
+++ Strings/AllCharactersSame/AllCharactersSame.java
@@ -0,0 +1,26 @@
+public class AllCharactersSame {
+
+  /** Driver Code */
+  public static void main(String[] args) {
+    System.out.println(isAllCharactersSame(""));
+    System.out.println(!isAllCharactersSame("aab"));
+    System.out.println(isAllCharactersSame("bbb"));
+    System.out.println(isAllCharactersSame("111"));
+  }
+
+  /**
+   * check if all the characters of a string are same
+   *
+   * @param str the string to check
+   * @return true if all characters of a string are same, otherwise false
+   */
+  public static boolean isAllCharactersSame(String str) {
+    for (int i = 1, length = str.length(); i < length; ++i) {
+      if (str.charAt(i) != str.charAt(0)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}

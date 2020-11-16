e078a73b0cf0cf750b72c3336cc988bf93f3e59b
Bunlong <bunlong.van@gmail.com>
2020-11-16 22:54:25 +0700

    :heavy_plus_sign: UpperCase

---
+++ Strings/UpperCase/UpperCase.java
@@ -0,0 +1,35 @@
+/**
+ * Expect
+ *
+ * toUpperCase("ABC") equals to "ABC".toUpperCase()
+ * toUpperCase("ABC123") equals to "ABC123".toUpperCase()
+ * toUpperCase("abcABC") equals to "abcABC".toUpperCase()
+ * toUpperCase("abc123ABC") equals to "abc123ABC".toUpperCase()
+ */
+public class UpperCase {
+
+  public static void main(String[] args) {
+    System.out.println(toUpperCase("ABC").equals("ABC".toUpperCase()));
+    System.out.println(toUpperCase("ABC123").equals("ABC123".toUpperCase()));
+    System.out.println(toUpperCase("abcABC").equals("abcABC".toUpperCase()));
+    System.out.println(toUpperCase("abc123ABC").equals("abc123ABC".toUpperCase()));
+  }
+
+  /**
+   * Convert the characters of the string into lower case
+   *
+   * @param str the string to convert
+   * @return the string converted to lowercase
+   */
+  public static String toUpperCase(String str) {
+    char[] chars = str.toCharArray();
+
+    for (int i = 0; i < chars.length; ++i) {
+      if (Character.isLetter(chars[i]) && Character.isLowerCase(chars[i])) {
+        chars[i] = Character.toUpperCase(chars[i]);
+      }
+    }
+
+    return new String(chars);
+  }
+}
